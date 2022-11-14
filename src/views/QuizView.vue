3bedb645bea2c2b050aa47783466ca2f4f633924
Paulina Cole <lina.cole@gmail.com>
2022-11-13 19:47:05 -0500

    working app

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,31 +1,49 @@
+<script setup>
+    import Question from "../components/Question.vue"
+    import QuizHeader from "../components/QuizHeader.vue"
+    import {useRoute} from "vue-router"
+    import { ref, computed } from "vue";
+    import quizes from "../data/data.json"
+    import Result from "../components/Results.vue"
+
+    const route = useRoute()
+    const quizId = parseInt(route.params.id);
+    const quiz = quizes.find(q => q.id === quizId)
+    const currentQuestionIndex = ref(0);
+    const numberOfCorrectAnswers = ref(0)
+    const showResults = ref(false)
+  
+    const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
+    const barPercentage = computed(() => `${currentQuestionIndex.value/quiz.questions.length * 100}%`)
+
+    const onOptionSelected = (isCorrect) => {
+        if(isCorrect){
+            numberOfCorrectAnswers.value++;
+        }
+        if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+          showResults.value = true
+        }
+        currentQuestionIndex.value++;
+    }
+</script>
+
 <template>
-  <div>
-    <header>
-      <h4>Question 1/3</h4>
-      <div class="bar">
-        <div class="completion">
-
+    <div>
+        <QuizHeader 
+            :questionStatus="questionStatus"
+            :barPercentage="barPercentage"
+        />
+        <div>
+            <Question
+                v-if="!showResults"
+                :question="quiz.questions[currentQuestionIndex]"
+                @selectOption="onOptionSelected"
+            />
+            <Result 
+              v-else
+              :quizQuestionLength="quiz.questions.length"
+              :numberOfCorrectAnswers="numberOfCorrectAnswers"
+              />
         </div>
-      </div>
-    </header>
-  </div>
+    </div>
 </template>
-
-<style scoped>
-  header {
-   margin-top: 20px;
-  }
-  header h4 {
-    font-size: 30px;
-  }
-  .bar {
-    width: 300px;
-    height: 50px;
-    border: 3px solid bisque;
-  }
-  .completion {
-    height: 100%;
-    width: 0%;
-    background-color: bisque;
-  }
-</style>
